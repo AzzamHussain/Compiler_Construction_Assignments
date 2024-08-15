46275b9cdc7248c0d91443b87ba9aea033d19a2e
AzzamHussain <azzamhussain70@gmail.com>
2024-08-15 23:19:33 +0500

    activity

--- Assignment01/regex_datatypes.py
+++ Assignment01/regex_datatypes.py
@@ -4,20 +4,20 @@ def data_type_finder(user_input):
     # Define regex patterns for data types
     integer_pattern = r"\d+"
     float_pattern = r"\d+\.\d+"
-    string_pattern = r"[a-zA-Z]+"
-    double_pattern = r"^\d*\.\d+$"
-    char_pattern = r"^.$"
+    # string_pattern = r"[a-zA-Z]+"
+    # double_pattern = r"^\d*\.\d+$"
+    # char_pattern = r"^.$"
 
     # Check for matches
     if re.fullmatch(integer_pattern, user_input):
         print("Input is an integer.")
     elif re.fullmatch(float_pattern, user_input):
         print("Input is a float.")
-    elif re.fullmatch(string_pattern, user_input):
-        print("Input is a string.")
-    elif re.fullmatch(double_pattern, user_input):
-        print("Input is a double.")
-    elif re.fullmatch(char_pattern, user_input):
+    # elif re.fullmatch(string_pattern, user_input):
+    #     print("Input is a string.")
+    # elif re.fullmatch(double_pattern, user_input):
+    #     print("Input is a double.")
+    # elif re.fullmatch(char_pattern, user_input):
         print("Input is a char.")
     else:
         print("Unable to determine the data type.")
